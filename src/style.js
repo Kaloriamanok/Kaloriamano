7fe96d328b591f902a088e89996449d60238403d
Loós András Péter <loosandraspeter@gmail.com>
2023-11-03 14:25:26 +0100

    added style.js for predefined className stylings

---
+++ src/style.js
@@ -0,0 +1,8 @@
+// ide kerülnek a tailwind egyesített osztályok
+// productionban már csak úgy néz ki hogy pl: className={styles.paragraph}
+
+const styles = {
+  paragraph: "font-normal text-marianBlue text-[18px] leading-[27px]",
+};
+
+export default styles;
