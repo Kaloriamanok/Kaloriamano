853cf22432523619d62c20eec09cc00f3bb2588b
Loós András Péter <loosandraspeter@gmail.com>
2023-11-05 12:05:50 +0100

    added general styles

--- src/style.js
+++ src/style.js
@@ -3,6 +3,16 @@
 
 const styles = {
   paragraph: "font-normal text-marianBlue text-[18px] leading-[27px]",
+
+  flexCenter: "flex justify-center items-center",
+  flexStart: "flex justify-center items-start",
+
+  paddingX: "sm:px-16 px-6",
+  paddingY: "sm:py-16 py-6",
+  padding: "sm:px-16 px-6 sm:py-12 py-4",
+
+  marginX: "sm:mx-16 mx-6",
+  marginY: "sm:my-16 my-6",
 };
 
 export default styles;
