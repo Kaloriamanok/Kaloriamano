f997b88051d0c7dd27e674c042be750eb4a9e4fc
Loós András Péter <loosandraspeter@gmail.com>
2023-11-08 21:59:04 +0100

    Added an example api call for future reference

---
+++ src/api/example.js
@@ -0,0 +1,19 @@
+/*  
+
+ Példa egy api hívásra
+
+*/
+
+const exampleApiCall = async (query) => {
+  const apiKey = "qBM788BqByOeR1WtBeX35g==jUYC642X1GCttFof";
+
+  return fetch(`https://api.calorieninjas.com/v1/nutrition?query=${query}`, {
+    headers: {
+      "X-Api-Key": apiKey,
+    },
+  })
+    .then((response) => response.json())
+    .catch((error) => console.error("Error:", error));
+};
+
+export default exampleApiCall;
---
+++ src/api/testExampleApiCall.js
@@ -0,0 +1,35 @@
+import exampleApiCall from "../api/example.js";
+
+const testQuery = "apple";
+
+exampleApiCall(testQuery)
+  .then((data) => console.log(data))
+  .catch((error) => console.error("Error:", error));
+
+/*
+    ezzel tesztelheted hogy az api hívás működik-e
+    NEM UNIT TESZTNEK SZÁMÍT, csak egy segítség
+
+    a terminálban navigálj el az api mappába, és futtasd a következő parancsot:
+    node testExampleApiCall.js
+
+    a következőt kell látnod a terminálban:
+    {
+  items: [
+    {
+      name: 'apple',
+      calories: 53,
+      serving_size_g: 100,
+      fat_total_g: 0.2,
+      fat_saturated_g: 0,
+      protein_g: 0.3,
+      sodium_mg: 1,
+      potassium_mg: 11,
+      cholesterol_mg: 0,
+      carbohydrates_total_g: 14.1,
+      fiber_g: 2.4,
+      sugar_g: 10.3
+    }
+  ]
+}
+  */
