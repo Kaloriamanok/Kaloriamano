f943d79666aa72688fc921c6074fd103582d1d70
rfannii <rajnafanny@gmail.com>
2023-11-10 21:00:40 +0100

    Footer elkészítése

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,49 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const creators = ["Loos András Péter", "Rajna Fanni", "Jenei Bence", "Gyüre Szabolcs"];
+
+  return (
+    <footer>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <h3 style={{ fontSize: "16px", marginBottom: "10px" }}>Készítők</h3>
+          <ul style={{ fontSize: "12px", listStyle: "none", padding: 0 }}>
+            {creators.map((creator, index) => (
+              <li key={index}>{creator}</li>
+            ))}
+          </ul>
+        </div>
+  
+        <div className="col-md-3">
+          <h3 style={{ fontSize: "16px", marginBottom: "20px" }}>Referenciák</h3>
+        </div>
+  
+        <div className="col-md-3">
+          <a
+            href="https://github.com/Kaloriamanok/Kaloriamano"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{
+              fontSize: "16px",
+              color: "#7FDBFF",
+              textDecoration: "none",
+              transition: "color 0.2s",
+              display: "block",
+              marginTop: "10px",
+            }}
+          >
+            Kalóriamanó GitHub oldal
+          </a>
+        </div>
+      </div>
+    </div>
+  </footer>
+  
+
+  );
 };
 
 export default Footer;
+
